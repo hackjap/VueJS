dbe6594bacf0a09ad45e2bbcd41c77419966e3d2
hackjap <hackjap1015@gmail.com>
2021-05-19 22:44:19 +0900

    Vue-CLI

---
+++ learn-vue-js/vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+// html의 표준태그와 충돌나지 않게 최소 두 단어의 파일명으로 생성한다
+
+<template>
+  <header>
+      <h1>{{propsdata} }</h1>
+      <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods:{
+        sendEvent:function(){
+            this.$emit('renew');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
