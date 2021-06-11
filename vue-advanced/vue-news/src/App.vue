3e52806d12912de4a37f39bb43fd1decffb98539
hackjap <hackjap1015@gmail.com>
2021-06-11 18:24:27 +0900

    라우터실전

--- vue-advanced/vue-news/src/App.vue
+++ vue-advanced/vue-news/src/App.vue
@@ -1,24 +1,35 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-  import ToolBar from './components/ToolBar.vue';
+import ToolBar from "./components/ToolBar.vue";
 
-  export default {
-    components: {
-      ToolBar
-    },
-
-  }
+export default {
+  components: {
+    ToolBar,
+  },
+};
 </script>
 
 <style>
-  body {
-    padding: 0;
-    margin: 0;
-  }
+body {
+  padding: 0;
+  margin: 0;
+}
+
+/* Router transition */
+.page-enter-active,
+.page_leave-active {
+  transition: opacity.7s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
 </style>
--- vue-advanced/vue-news/src/views/ItemView.vue
+++ vue-advanced/vue-news/src/views/ItemView.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-    <p>{{ itemInfo.title }}</p>
+    <section>
+      <!-- 질문 상세 영역 -->
+      <div class="user-container">
+        <div>
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <router-link :to="`/user/${fetchedItem.user}`">
+            {{ fetchedItem.user }}
+          </router-link>
+          <div class="time">
+            {{ fetchedItem.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2>{{ fetchedItem.title }}</h2>
+    </section>
+    <section>
+      <!-- 질문 댓글 -->
+      <div v-html="fetchedItem.content">
+        <!-- {{fetchedItem.content}} -->
+      </div>
+    </section>
+
+    <!-- <p>{{ fetchedItem.title }}</p>
     <div>
-      {{ itemInfo.content }}
-    </div>
+      {{ fetchedItem.content }}
+    </div> -->
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-  computed: {},
+  computed: {
+    ...mapGetters(["fetchedItem"]),
+  },
 
   created() {
     const itemId = this.$route.params.id;
@@ -18,5 +46,20 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
 </style>
