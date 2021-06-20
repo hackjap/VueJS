860a9b9d2f5a3080d1bccd129117a50d315a2f77
hackjap <hackjap1015@gmail.com>
2021-06-20 12:19:31 +0900

    서비스 배포 환경 구성

--- vue-advanced/vue-news/src/App.vue
+++ vue-advanced/vue-news/src/App.vue
@@ -34,6 +34,7 @@ export default {
   created() {
     bus.$on("start:spinner", this.startSpinner);
     bus.$on("end:spinner", this.endSpinner);
+    console.log(process.env.VUE_APP_TITLE);
   },
   beforeDestroy() {
     bus.$off("start:spinner", this.startSpinner);
